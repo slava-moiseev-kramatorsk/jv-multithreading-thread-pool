8af4334d8c6c82309963a018d1eb7261be51d457
Slava_Moiseev <qualityengineerinkom@gmail.com>
2025-09-01 21:42:11 +0300

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,8 +12,16 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        MyThread myThread = new MyThread();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+        for (Future<String> future : futures) {
+            logger.info(future.get());
+        }
+        executorService.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = ThreadLocalRandom.current().nextInt(100, 500);
+        Thread.sleep(duration);
+        return "Task duration was " + duration + " ms, execution finished at " + LocalTime.now();
     }
 }
